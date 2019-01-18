b2f3b80934a0f08b009e4e8ce4602cee1040e922
Luke Busby <labusby1@gmail.com>
2019-01-18 15:52:43 +0000

    Added portfolio object table and connected user with portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_150138) do
+ActiveRecord::Schema.define(version: 2019_01_18_154944) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.float "balance"
+    t.float "net_worth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
