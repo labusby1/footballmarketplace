aa5b2e39cdf4bd70d00be542f4fd95431e597c18
Luke Busby <labusby1@gmail.com>
2019-01-25 19:54:40 +0000

    Mid player creation in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,25 @@
 #-----------------------------------Seeds---------------------------------------
 
+
+
+
+
+
+#-------------------------------------------------------------------------------
+#------------------------------Beings-------------------------------------------
 Being.create(family: "League", can_be_owned: false)
 Being.create(family: "Club", can_be_owned: true)
 Being.create(family: "Player", can_be_owned: true)
 Being.create(family: "Manager", can_be_owned: true)
+
+#------------------------------Possessions--------------------------------------
 club = Possession.create(being_id: Being.find_by(family: "Club").id)
 player = Possession.create(being_id: Being.find_by(family: "Player").id)
 manager = Possession.create(being_id: Being.find_by(family: "Manager").id)
+
+#------------------------------Leagues------------------------------------------
 epl = League.create(name: "Premier League", country: "England/Wales", being_id: Being.find_by(family: "League").id)
-arsenal = Club.create(name: "Arsenal", location: "North London", founded: "1900", possession_id: club.id)
-epl.clubs << arsenal
 championsleague = League.create(name: "Champions League", country: "Europe", being_id: 1)
-chelsea = Club.create(name: "Chelsea", location: "East London", founded: "1901", possession_id: club.id)
-epl.clubs << chelsea
-championsleague.clubs << chelsea
-
-Player.create(name: "Ozil", nationality: "Germany", dob: "1989", club_id: arsenal.id, possession_id: player.id)
-#arsenal.players << ozil
-
-Manager.create(name: "Emery",nationality: "Spain", dob: "1975",club_id: arsenal.id, possession_id: manager.id)
-
-
-Stock.create(stock_name: "Arsenal", symbol: "ARS", active: false, trade_type: "Club", portfolio_id: nil, possession_id: club.id)
 
 #-------------------------------------------------------------------------------
 # https://raw.githubusercontent.com/drraq/PremierLeague.json/master/data.json
@@ -37,4 +35,13 @@ file = File.read(doc)
 data_hash = JSON.parse(file)
 for i in 0..19
   Club.create(name: data_hash["clubs"][i]["name"], location: data_hash["clubs"][i]["stadium"], founded: "2019", possession_id: club.id)
+  epl.clubs << Club.find(i+1)
+end
+
+#-- arsenal players --
+num_of_players = data_hash["players"].length
+for i in 0..num_of_players
+  if data_hash["players"][i]["current_club"] == "Arsenal"
+    #Player.create(name: data_hash["players"][i]["last_name"], nationality: data_hash["players"][i]["nationality"], dob: "2019", club_id: Club.find_by(name: "Arsenal").id, possession_id: player.id)
+  end
 end
