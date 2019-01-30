f06a92bf7c8476c6330cdbc7234c6799a6ceef16
Luke Busby <labusby1@gmail.com>
2019-01-30 15:48:06 +0000

    Added VERY basic create a portfolio form

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,5 +3,6 @@ class PortfoliosController < ApplicationController
   #GET request for /users/:user_id/portfolio/new
   def new
     # Render black portfolio form
+    @portfolio = Portfolio.new
   end
 end
