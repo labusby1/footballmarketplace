f069a4aaeb41f24f481ef03a16fb04525207a809
Luke Busby <labusby1@gmail.com>
2019-01-31 21:06:55 +0000

    Commit before I go back and change routes to become nested using the unrecomended way

--- app/controllers/onthemarkets_controller.rb
+++ app/controllers/onthemarkets_controller.rb
@@ -5,6 +5,23 @@ class OnthemarketsController < ApplicationController
     @onthemarket = Onthemarket.new
   end
   
+  #POST request to /users/:user_id/portfolio/onthemarkets
+  def create
+    # Ensure that we have the user that fills out form
+    @user = User.find(params[:user_id])
+    # Create onthemarket linked to specific user
+    @onthemarket = @user.build_onthemarket(onthemarket_params)
+    if @onthemarket.save
+      flash[:success] = "Onthemarket Created!"
+      redirect_to root_path
+    else 
+      render action: :new
+    end
+  end
   
-  
+  private
+  #white list the onthemarket parameters
+    def onthemarket_params
+      params.require(:onthemarket).permit(:buying, :portfolio_id, :stocks_on_market, :least_possible)
+    end
 end
