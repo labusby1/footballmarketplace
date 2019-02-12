ad86f34b1b21dc93e2932ddcf1b79c8adef94f49
Luke Busby <labusby1@gmail.com>
2019-02-12 15:57:48 +0000

    Added very basic transaction cost display

--- app/assets/javascripts/transactors.js
+++ app/assets/javascripts/transactors.js
@@ -4,23 +4,31 @@ $(document).on('turbolinks:load', function(){
   
   var theForm = $('#new-transactor-form');
   var submitBtn = $('#form-submit-btn');
-  var pricePer = $('#price-per-transactor-form')
-  var priceBtn = $('#calculate-transaction')
-  
+  var pricePer = $('#price-per-transactor-form').get(0).dataset.pricePer;
+  var priceBtn = $('#calculate-transaction');
+
   //Target transaction quantity input box.
-  var stockQuantity = $('#transaction-quantity').val()
-  
-  //When a user clicks the calculate cost button,
-  
-  //The quantity * price_per will show the total transaction cost.
-  
-  //When user clicks submit,
-  submitBtn.click(function(event){
-     //we prevent deault behaviour, pausing the form.
-    event.preventDefault();
-    //Inject the quantity * price_per float value into transaction_cost.
-    stockQuantity = $('#transaction-quantity').val()
+  var stockQuantityBox = $('#transaction-quantity');
+  stockQuantityBox.change(function(){
+    //The quantity * price_per will show the total transaction cost.
+    var stockQuantity = $('#transaction-quantity').val();
+    var outputted = stockQuantity * pricePer;
+    var para = document.createElement("P");                       // Create a <p> element
+    var t = document.createTextNode(outputted);       // Create a text node
+    para.appendChild(t);                                  // Append the text to <p>
+    document.getElementById("calculate-transaction").appendChild(para); 
   });
-  //Submit form to rails app.
+  
+  
+  
+  // //When user clicks submit,
+  // submitBtn.click(function(event){
+  //   //we prevent deault behaviour, pausing the form.
+  //   event.preventDefault();
+  //   submitBtn.val("Processing").prop('disabled', true);
+  //   //Inject the quantity * price_per float value into transaction_cost.
+  //   stockQuantity = $('#transaction-quantity').val();
+  // });
+  // //Submit form to rails app.
   
 });
