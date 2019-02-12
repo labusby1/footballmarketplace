d49fea01daa8e3231a0703a89987afb36ecb1173
Luke Busby <labusby1@gmail.com>
2019-02-12 16:37:25 +0000

    Added feature that when a new transaction quantity is updated, the previous costcalc is removed

--- app/assets/javascripts/transactors.js
+++ app/assets/javascripts/transactors.js
@@ -5,30 +5,30 @@ $(document).on('turbolinks:load', function(){
   var theForm = $('#new-transactor-form');
   var submitBtn = $('#form-submit-btn');
   var pricePer = $('#price-per-transactor-form').get(0).dataset.pricePer;
-  var priceBtn = $('#calculate-transaction');
 
   //Target transaction quantity input box.
   var stockQuantityBox = $('#transaction-quantity');
   stockQuantityBox.change(function(){
+    document.getElementById("calculate-transaction").innerHTML = '';
     //The quantity * price_per will show the total transaction cost.
     var stockQuantity = $('#transaction-quantity').val();
     var outputted = stockQuantity * pricePer;
     var para = document.createElement("P");                       // Create a <p> element
-    var t = document.createTextNode(outputted);       // Create a text node
+    var t = document.createTextNode(outputted);  // Create a text node
     para.appendChild(t);                                  // Append the text to <p>
     document.getElementById("calculate-transaction").appendChild(para); 
   });
   
   
   
-  // //When user clicks submit,
-  // submitBtn.click(function(event){
-  //   //we prevent deault behaviour, pausing the form.
-  //   event.preventDefault();
-  //   submitBtn.val("Processing").prop('disabled', true);
-  //   //Inject the quantity * price_per float value into transaction_cost.
-  //   stockQuantity = $('#transaction-quantity').val();
-  // });
-  // //Submit form to rails app.
+  //When user clicks submit,
+  submitBtn.click(function(event){
+    //we prevent deault behaviour, pausing the form.
+    event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
+    //Inject the quantity * price_per float value into transaction_cost.
+    var stockQuantity = $('#transaction-quantity').val();
+  });
+  //Submit form to rails app.
   
 });
