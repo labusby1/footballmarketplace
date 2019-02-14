292855591cd309e5b91dd9b9e1ef99455176ae7f
Luke Busby <labusby1@gmail.com>
2019-02-14 14:43:54 +0000

    Going to create a new table to store transaction stocks in

--- app/assets/javascripts/transactors.js
+++ app/assets/javascripts/transactors.js
@@ -9,7 +9,9 @@ $(document).on('turbolinks:load', function(){
   //Target transaction quantity input box.
   var stockQuantityBox = $('#transaction-quantity');
   stockQuantityBox.change(function(){
+    //clear previous calculation from the div insides
     document.getElementById("calculate-transaction").innerHTML = '';
+    
     //The quantity * price_per will show the total transaction cost.
     var stockQuantity = $('#transaction-quantity').val();
     var outputted = stockQuantity * pricePer;
@@ -26,8 +28,31 @@ $(document).on('turbolinks:load', function(){
     //we prevent deault behaviour, pausing the form.
     event.preventDefault();
     submitBtn.val("Processing").prop('disabled', true);
+    
     //Inject the quantity * price_per float value into transaction_cost.
     var stockQuantity = $('#transaction-quantity').val();
+    var outputted = stockQuantity * pricePer;
+    var costOutputBox = $('#calculate-transaction');
+    //theForm.append($('<%= f.hidden_field :transaction_cost, :value =>'+ outputted +' %>' ));
+    costOutputBox.append($('<input value="'+outputted+'" type="hidden" name="transactor[transaction_cost]", id="transactor_transaction_cost">'));
+    //theForm.append($('<input type="hidden" name="transactor[transaction_cost]">').val(outputted) );
+    
+    //Here I am inserting ruby code for each stockQuantity integer building the moveable array 
+    var moveableOutputBox = $('#moveable-stocks');
+    var array_of_ids = $('#array_of_idsJS').get(0).dataset.array;
+    
+    
+//     {
+// "name":"John",
+// "age":30,
+// "cars":[ "Ford", "BMW", "Fiat" ]
+// }
+    // for (var i=1; i < array_of_ids.length - 1; i++){
+    //   moveableOutputBox.append($('<input type="hidden" name="transactor[moveable][]['+array_of_ids.get(i)+']>'));
+    // };
+    
+    //Continue with form submition
+    theForm.eq(0).submit();
   });
   //Submit form to rails app.
   
--- app/controllers/transactors_controller.rb
+++ app/controllers/transactors_controller.rb
@@ -2,4 +2,20 @@ class TransactorsController < ApplicationController
   def new
     @transactor = Transactor.new
   end
+  
+  def create 
+    # @buyer = Buyer.create(portfolio_id: User.find(params[:user_id]).portfolio.id, purchase_price: 1)
+    @transactor = Transactor.new(transactor_params)
+    # @transactor.moveable = @array_of_ids
+    if @transactor.save
+      flash[:success] = "Transactor Created!"
+      redirect_to user_path(id: params[:user_id])
+    else
+      render root_path
+    end
+  end
+  private
+    def transactor_params
+      params.require(:transactor).permit(:buyer_id, :seller_id, :transaction_cost, :moveable)
+    end
 end
