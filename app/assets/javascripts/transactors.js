eaa386418a09a54fdc064af928728fb3d21f0301
Luke Busby <labusby1@gmail.com>
2019-02-15 19:48:50 +0000

    Added comments for the transactors js, form and controller

--- app/assets/javascripts/transactors.js
+++ app/assets/javascripts/transactors.js
@@ -34,17 +34,10 @@ $(document).on('turbolinks:load', function(){
     var outputted = stockQuantity * pricePer;
     var costOutputBox = $('#calculate-transaction');
     costOutputBox.append($('<input value="'+outputted+'" type="hidden" name="transactor[transaction_cost]", id="transactor_transaction_cost">'));
+    // Also injecting the stockQuantity value into the transactor form as moveable
     costOutputBox.append($('<input value="'+stockQuantity+'" type="hidden" name="transactor[moveable]", id="transactor_moveable">'));
-    //theForm.append($('<input type="hidden" name="transactor[transaction_cost]">').val(outputted) );
-
-    //var array_of_stocks = $('#stocks-to-move-input').val();
-    // var numberOutputBox = $('#stocks-to-move-output-box');
-    // numberOutputBox.append($('<%= @array_of_ids = Stock.where(symbol: @otmobj.stocks_on_market, portfolio_id: @otmobj.portfolio_id).ids.take()%>'));
-    // debugger;
-    // numberOutputBox.append('<%= js_obj = @array_of_ids.to_json %>');
-    // numberOutputBox.append($('<input value= @array_of_ids.to_json type="hidden" name="transactor[stocks_to_move]", id="transactor_stocks_to_move">'));
     
-     //<%= content_tag :div, id: "stocks-to-move-input", data: {js_array_to_manipulate: js_array_of_ids} do %><% end %>
+    //continues with form submittion
     theForm.eq(0).submit();
   });
   //Submit form to rails app.
--- app/controllers/transactors_controller.rb
+++ app/controllers/transactors_controller.rb
@@ -7,17 +7,20 @@ class TransactorsController < ApplicationController
     # @buyer = Buyer.create(portfolio_id: User.find(params[:user_id]).portfolio.id, purchase_price: 1)
     @transactor = Transactor.new(transactor_params)
     if @transactor.save
+      #After a sucessful save, funds are transfered, stocks are tramsfered and the onthemarket object that the transactor was built from, gets deleted
       flash[:success] = "Transactor Created!"
       @transactor.transfer_funds()
       @transactor.transfer_stocks()
-      redirect_to user_path(id: params[:user_id])
       Onthemarket.find(@transactor.otm_id).delete
+      #Redirect the user to their portfolio
+      redirect_to user_path(id: params[:user_id])
     else
       render root_path
     end
   end
   private
     def transactor_params
+      #Whitelist parameters of the transactor
       params.require(:transactor).permit(:buyer_id, :seller_id, :transaction_cost, :stocks_to_move, :moveable, :otm_id)
     end
 end
