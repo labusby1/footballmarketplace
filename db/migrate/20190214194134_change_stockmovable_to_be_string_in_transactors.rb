da4ae9fdb108a5aa042f6c98893fddabe80101d8
Luke Busby <labusby1@gmail.com>
2019-02-25 15:07:47 +0000

    Again similar problem and solution to last commit

--- db/migrate/20190214194134_change_stockmovable_to_be_string_in_transactors.rb
+++ db/migrate/20190214194134_change_stockmovable_to_be_string_in_transactors.rb
@@ -1,6 +1,5 @@
 class ChangeStockmovableToBeStringInTransactors < ActiveRecord::Migration[5.2]
   def change
-    remove_column :transactors, :stocks_to_move
     add_column :transactors, :stocks_to_move, :text
   end
 end
