92d82ecac392e87a424ae37a8090fab3d9ff4212
Luke Busby <labusby1@gmail.com>
2019-03-01 18:42:34 +0000

    Select box on portfolio page now creates a chart for selected option

--- app/assets/javascripts/usersShow.js
+++ app/assets/javascripts/usersShow.js
@@ -4,33 +4,33 @@ $(document).on('turbolinks:load', function(){
   if ($('meta[name=psj]').attr('controller') == 'users' && $('meta[name=psj]').attr('action') == 'show'){
     
     var outputBox = $('#chart-output');
-    var parentEl = $('#transactor_chart');
+    // var parentEl = $('#transactor_chart');
     //Need to listen for a user to select a stock they wish to be charted 
     $('#select-a-stock').change(function(){
-      var stockSelected = $(this).value;
+      var stockSelected = $(this).val();
+      var stringStockSelected = '#transactor_chart_'+stockSelected;
+      // var parentEl = outputBox;
       
       // Clear all children html nodes for the transactor_chart element where. This clears the previous 
       //selected chart.
-      while (parentEl[0].firstChild) {
-        parentEl[0].firstChild.remove();
+      while (outputBox[0].firstChild) {
+        outputBox[0].firstChild.remove();
       }
       new Morris.Line({
         // ID of the element in which to draw the chart.
-        element: 'transactor_chart',
+        element: 'chart-output',
         // Chart data records -- each entry in this array corresponds to a point on
         // the chart.
-        data: $('#transactor_chart').data('transactors'),
+        data: $(stringStockSelected).data('transactors'),
         // The name of the data record attribute that contains x-values.
         xkey: 'sold_on',
         // A list of names of data record attributes that contain y-values.
-        ykeys: ['transaction_cost'],
+        ykeys: ['price_per'],
         // Labels for the ykeys -- will be displayed when you hover over the
         // chart.
-        labels: ['Transaction_Cost']
+        labels: ['Price per stock']
       });
     });
-    
-    
   }
   
   else {
