1bced8dfff8f25405aef8c0fe9d082a945a5b9de
Luke Busby <labusby1@gmail.com>
2019-03-01 20:36:48 +0000

    Midway through implementing default morris chart on portfolio page

--- app/assets/javascripts/usersShow.js
+++ app/assets/javascripts/usersShow.js
@@ -4,7 +4,27 @@ $(document).on('turbolinks:load', function(){
   if ($('meta[name=psj]').attr('controller') == 'users' && $('meta[name=psj]').attr('action') == 'show'){
     
     var outputBox = $('#chart-output');
-    // var parentEl = $('#transactor_chart');
+    var stringStockDefault = '#transactor_chart_'+ $('#mvs-string-tag').data('transactors');
+    
+    //Default chart to create is the most valuable which was determined in the show file.
+    new Morris.Line({
+      // ID of the element in which to draw the chart.
+      element: 'chart-output',
+      // Chart data records -- each entry in this array corresponds to a point on
+      // the chart.
+      data: $(stringStockDefault).data('transactors'),
+      // The name of the data record attribute that contains x-values.
+      xkey: 'sold_on',
+      // A list of names of data record attributes that contain y-values.
+      ykeys: ['price_per'],
+      // Labels for the ykeys -- will be displayed when you hover over the
+      // chart.
+      labels: ['Price per stock']
+    });
+    
+    
+    
+    
     //Need to listen for a user to select a stock they wish to be charted 
     $('#select-a-stock').change(function(){
       var stockSelected = $(this).val();
